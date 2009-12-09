0be16e6649261e1462d2b48442bf4497c833547f
Antoine Beaupre <anarcat@koumbit.org>
2009-12-08 22:56:41 -0500

    allow local fileserver distribution instead of module-specific files

--- manifests/base.pp
+++ manifests/base.pp
@@ -14,8 +14,8 @@ class denyhosts::base  {
     }
 
     file{'/etc/denyhosts.conf':
-        source => [ "puppet://$server/modules/site-denyhosts/${fqdn}/denyhosts.conf", 
-                    "puppet://$server/modules/site-denyhosts/denyhosts.conf",
+        source => [ "puppet://$server/files/${fqdn}/denyhosts.conf", 
+                    "puppet://$server/files/denyhosts.conf",
                     "puppet://$server/modules/denyhosts/denyhosts.conf" ],
         notify => Service[denyhosts],
         mode => 0600, owner => root, group => 0;
@@ -26,8 +26,8 @@ class denyhosts::base  {
         owner => root, group => 0, mode => 0700; 
     }
     file{'/var/lib/denyhosts/allowed-hosts':
-        source => [ "puppet://$server/modules/site-denyhosts/${fqdn}/allowed-hosts", 
-                    "puppet://$server/modules/site-denyhosts/allowed-hosts",
+        source => [ "puppet://$server/files/${fqdn}/allowed-hosts", 
+                    "puppet://$server/files/allowed-hosts",
                     "puppet://$server/modules/denyhosts/allowed-hosts" ],
         notify => Service[denyhosts],
         mode => 0600, owner => root, group => 0;
