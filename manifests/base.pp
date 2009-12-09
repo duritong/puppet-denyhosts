34b76b361f12d1dd7291674528682448ca29d0c9
Antoine Beaupre <anarcat@koumbit.org>
2009-12-08 22:57:46 -0500

    whitespace cleanup

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,11 +1,11 @@
 class denyhosts::base  {
-    package{denyhosts:
+    package { denyhosts:
         ensure => present,
         require => [ File["/etc/denyhosts.conf"],
             File["/var/lib/denyhosts/allowed-hosts"] ],
     }
 
-    service{denyhosts:
+    service { denyhosts:
         enable => true,
         ensure => running,
         require => [ Package[denyhosts], 
@@ -13,23 +13,20 @@ class denyhosts::base  {
             File["/var/lib/denyhosts/allowed-hosts"] ],
     }
 
-    file{'/etc/denyhosts.conf':
-        source => [ "puppet://$server/files/${fqdn}/denyhosts.conf", 
-                    "puppet://$server/files/denyhosts.conf",
-                    "puppet://$server/modules/denyhosts/denyhosts.conf" ],
-        notify => Service[denyhosts],
-        mode => 0600, owner => root, group => 0;
-    }
-
-    file{'/var/lib/denyhosts':
-        ensure => directory,
-        owner => root, group => 0, mode => 0700; 
-    }
-    file{'/var/lib/denyhosts/allowed-hosts':
-        source => [ "puppet://$server/files/${fqdn}/allowed-hosts", 
-                    "puppet://$server/files/allowed-hosts",
-                    "puppet://$server/modules/denyhosts/allowed-hosts" ],
-        notify => Service[denyhosts],
-        mode => 0600, owner => root, group => 0;
+    file { '/etc/denyhosts.conf':
+            source => [ "puppet://$server/files/${fqdn}/denyhosts.conf", 
+                        "puppet://$server/files/denyhosts.conf",
+                        "puppet://$server/modules/denyhosts/denyhosts.conf" ],
+            notify => Service[denyhosts],
+            mode => 0600, owner => root, group => 0;
+        '/var/lib/denyhosts':
+            ensure => directory,
+            owner => root, group => 0, mode => 0700; 
+        '/var/lib/denyhosts/allowed-hosts':
+            source => [ "puppet://$server/files/${fqdn}/allowed-hosts", 
+                        "puppet://$server/files/allowed-hosts",
+                        "puppet://$server/modules/denyhosts/allowed-hosts" ],
+            notify => Service[denyhosts],
+            mode => 0600, owner => root, group => 0;
     }
 }
