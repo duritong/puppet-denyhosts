e6670b856c95346b5d97ca53a5a6ed2aac857624
mh <mh@immerda.ch>
2010-08-07 02:12:14 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/base.pp
+++ manifests/base.pp
@@ -14,20 +14,20 @@ class denyhosts::base  {
     }
 
     file { '/etc/denyhosts.conf':
-            source => [ "puppet://$server/modules/site-denyhosts/${fqdn}/denyhosts.conf",
-                        "puppet://$server/modules/site-denyhosts/${operatingsystem}/denyhosts.conf",
-                        "puppet://$server/modules/site-denyhosts/denyhosts.conf",
-                        "puppet://$server/modules/denyhosts/${operatingsystem}/denyhosts.conf",
-                        "puppet://$server/modules/denyhosts/denyhosts.conf" ],
+            source => [ "puppet:///modules/site-denyhosts/${fqdn}/denyhosts.conf",
+                        "puppet:///modules/site-denyhosts/${operatingsystem}/denyhosts.conf",
+                        "puppet:///modules/site-denyhosts/denyhosts.conf",
+                        "puppet:///modules/denyhosts/${operatingsystem}/denyhosts.conf",
+                        "puppet:///modules/denyhosts/denyhosts.conf" ],
             notify => Service[denyhosts],
             mode => 0600, owner => root, group => 0;
         '/var/lib/denyhosts':
             ensure => directory,
             owner => root, group => 0, mode => 0700; 
         '/var/lib/denyhosts/allowed-hosts':
-            source => [ "puppet://$server/modules/site-denyhosts/${fqdn}/allowed-hosts", 
-                        "puppet://$server/modules/site-denyhosts/allowed-hosts",
-                        "puppet://$server/modules/denyhosts/allowed-hosts" ],
+            source => [ "puppet:///modules/site-denyhosts/${fqdn}/allowed-hosts", 
+                        "puppet:///modules/site-denyhosts/allowed-hosts",
+                        "puppet:///modules/denyhosts/allowed-hosts" ],
             notify => Service[denyhosts],
             mode => 0600, owner => root, group => 0;
     }
