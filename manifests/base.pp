2d10b62fe6d5f73881a1040e3fd89cea233587ac
mh <mh@immerda.ch>
2014-02-06 12:00:44 +0100

    linting

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,37 +1,44 @@
+# the basic setup
 class denyhosts::base  {
-  package { denyhosts:
-    ensure => present,
-    require => [ File["/etc/denyhosts.conf"],
-      File["/var/lib/denyhosts/allowed-hosts"] ],
+  package {'denyhosts':
+    ensure  => present,
+    require => [ File['/etc/denyhosts.conf'],
+      File['/var/lib/denyhosts/allowed-hosts'] ],
   }
 
-  service { denyhosts:
-    enable => true,
-    ensure => running,
-    require => [ Package[denyhosts],
-      File["/etc/denyhosts.conf"],
-      File["/var/lib/denyhosts/allowed-hosts"] ],
+  service {'denyhosts':
+    ensure  => running,
+    enable  => true,
+    require => [ Package['denyhosts'],
+      File['/etc/denyhosts.conf'],
+      File['/var/lib/denyhosts/allowed-hosts'] ],
   }
 
   file {
     '/etc/denyhosts.conf':
-      source => [ "puppet:///modules/site_denyhosts/${::fqdn}/denyhosts.conf",
-                 "puppet:///modules/site_denyhosts/${::operatingsystem}/denyhosts.conf",
-                  "puppet:///modules/site_denyhosts/denyhosts.conf",
+      source  => ["puppet:///modules/site_denyhosts/${::fqdn}/denyhosts.conf",
+                  "puppet:///modules/site_denyhosts/${::operatingsystem}/denyhosts.conf",
+                  'puppet:///modules/site_denyhosts/denyhosts.conf',
                   "puppet:///modules/denyhosts/${::operatingsystem}/denyhosts.conf",
-                  "puppet:///modules/denyhosts/denyhosts.conf" ],
-      notify => Service[denyhosts],
-      mode => 0600, owner => root, group => 0;
+                  'puppet:///modules/denyhosts/denyhosts.conf' ],
+      notify  => Service['denyhosts'],
+      owner   => root,
+      group   => 0,
+      mode    => '0600';
     '/var/lib/denyhosts':
-      ensure => directory,
-      before => Package[denyhosts],
-      owner => root, group => 0, mode => 0700;
+      ensure  => directory,
+      before  => Package['denyhosts'],
+      owner   => root,
+      group   => 0,
+      mode    => '0700';
     '/var/lib/denyhosts/allowed-hosts':
-      source => "puppet:///modules/denyhosts/allowed-hosts",
+      source  => 'puppet:///modules/denyhosts/allowed-hosts',
       replace => false,
-      before => Package[denyhosts],
-      notify => Service[denyhosts],
-      mode => 0600, owner => root, group => 0;
+      before  => Package['denyhosts'],
+      notify  => Service['denyhosts'],
+      owner   => root,
+      group   => 0,
+      mode    => '0600';
   }
 
   Denyhosts::Allowed_host <<||>>
